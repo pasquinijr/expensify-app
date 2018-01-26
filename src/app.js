34991b7746d9be062d12596a66c544b5ff527b71
Jose Pasquini <jose.pasquini@vistajet.com>
2018-01-26 14:20:12 -0200

    end of section 11

--- src/app.js
+++ src/app.js
@@ -12,9 +12,9 @@ import { setTimeout } from 'timers';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 1517407200000}));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 44500, createdAt: 1514815200000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 1516197600000}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,10 +1,11 @@
+import moment from 'moment';
 // Filters Reducer
 
 const filtersReducersDefaultState = {
   text: '',
   sortBy: 'date',
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month')
 };
 
 export default (state = filtersReducersDefaultState, action) => {
