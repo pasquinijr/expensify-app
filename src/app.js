82dc156286625af9ea21af3308223caf56093593
Jose Pasquini Jr <pasquinijr@gmail.com>
2018-01-31 15:27:42 -0200

    end of section 12

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 import { setTimeout } from 'timers';
 
 const store = configureStore();
